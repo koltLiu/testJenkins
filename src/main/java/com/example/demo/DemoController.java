21fb0b4964555c802d62b9a9566add3d3ba01841
kolt.liu <kolt.liu@quectecl.com>
2023-03-22 16:45:44 +0800

    我的第一次提交

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+
+    @GetMapping("hello/{str}")
+    public String hello(@PathVariable String str){
+        return "hello:"+str;
+    }
+}
