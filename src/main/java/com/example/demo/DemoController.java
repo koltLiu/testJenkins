d8c400d0bcf878fc4d04558bf63d4c273f815b3b
kolt.liu <kolt.liu@quectecl.com>
2023-03-28 15:25:31 +0800

    我的提交测试123111

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Calendar;
+
 @RestController
 @RequestMapping("/")
 public class DemoController {
 
     @GetMapping("hello/{str}")
     public String hello(@PathVariable String str){
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.get(Calendar.HOUR_OF_DAY);
         return "hello:"+str;
     }
 
